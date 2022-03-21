19dd0848a98de9d2b29ecc39b328514044d0bd53
MaximoEspitia <max19espitia@gmail.com>
2022-03-21 16:43:05 -0500

    commit tree be05e7a9004329c1cee7bc7fcf8b8905cee40e0a

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+//const sumOfBatches = 0
+
+const totalBatteries = batteryBatches.reduce(function(oldVal, newVal) {
+    console.log(totalBatteries); 
+    return oldVal + newVal;
+});
+
+//console.log(totalBatteries);
+  //for( let i = 0, i < batteryBatches.length; i++) {
+      //umOfBatches += batteryBatches[i];
+  //}
+  //console.log(sumofBatches);
+
+
 // Code your solution here
+//function getTotal(numbers) {
+//const totalBatteries = 0 
+//for (const total of batteryBatches) {
+  //  totalBatteries += total.batteryBatches;
+//};
+//return totalBatteries;
+//}
+//console.log(getTotal(batteryBatches));
+
+
+
